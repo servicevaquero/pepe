5b3c572595576a3751956d04366715632dcb7b7c
alangauna <alangauna@hotmail.com.ar>
2013-10-08 17:34:33 -0300

    Vista clientes

---
+++ src/main/scala/applicationModel/ABMClientes.scala
@@ -0,0 +1,5 @@
+package applicationModel
+
+class ABMClientes {
+
+}
---
+++ src/main/scala/ui/ABMClientesWindow.scala
@@ -0,0 +1,98 @@
+package ui
+
+import domain._
+import applicationModel.ABMClientes
+
+import org.uqbar.arena.windows.Dialog
+import org.uqbar.arena.windows.WindowOwner
+import collection.JavaConversions._
+import java.awt.Color
+import org.uqbar.arena.layout.ColumnLayout
+import org.uqbar.arena.layout.HorizontalLayout
+import org.uqbar.arena.bindings.NotNullObservable
+import org.uqbar.arena.bindings.ObservableProperty
+import org.uqbar.arena.bindings.PropertyAdapter
+import org.uqbar.arena.widgets.CheckBox
+import org.uqbar.arena.widgets.Label
+import org.uqbar.arena.widgets.Panel
+import org.uqbar.arena.widgets.Selector
+import org.uqbar.arena.widgets.TextBox
+import org.uqbar.arena.windows.Dialog
+import org.uqbar.arena.windows.WindowOwner
+import org.uqbar.arena.layout.ColumnLayout
+import org.uqbar.commons.utils.ApplicationContext
+import org.uqbar.arena.widgets.Button
+import org.uqbar.arena.actions.MessageSend
+import collection.JavaConversions._
+import org.uqbar.arena.widgets.tables.Column
+import org.uqbar.arena.widgets.tables.Table
+
+
+class ABMClientesWindow(parent: WindowOwner, unCliente : Cliente) extends Dialog[ABMClientes](parent, new ABMClientes) {
+ 
+ //getModelObject.search()
+  
+ override def createMainTemplate(mainPanel: Panel) = {
+    this.setTitle("Clientes")
+    this.setTaskDescription("Lista de clientes")
+
+    super.createMainTemplate(mainPanel)
+
+    this.createResultsGrid(mainPanel)
+    this.createGridActions(mainPanel)
+  }
+
+   override def createFormPanel(mainPanel: Panel) = {
+    var form = new Panel(mainPanel)
+    form.setLayout(new ColumnLayout(2))
+   }
+   
+   def createResultsGrid(mainPanel: Panel) {
+    var table = new Table[Cliente](mainPanel, classOf[Cliente])
+    table.setHeigth(200)
+    table.setWidth(550)
+    table.bindItemsToProperty("clientes")
+    table.bindValueToProperty("clienteSeleccionado")
+    this.describeResultsGrid(table)
+   }
+   
+   def describeResultsGrid(table: Table[Cliente]) {
+    new Column[Cliente](table)
+      .setTitle("Nombre y Apellido")
+      .setFixedSize(100)
+      .bindContentsToProperty("nombre")
+      
+      new Column[Cliente](table)
+      .setTitle("DNI")
+      .setFixedSize(100)
+      .bindContentsToProperty("dni")
+
+    new Column[Cliente](table)
+      .setTitle("Edad")
+      .setFixedSize(100)
+      .bindContentsToProperty("edad")
+      
+      new Column[Cliente](table)
+      .setTitle("Sexo")
+      .setFixedSize(100)
+      .bindContentsToProperty("sexo")
+   }
+
+ def createGridActions(mainPanel: Panel) {
+    var actionsPanel = new Panel(mainPanel)
+    actionsPanel.setLayout(new HorizontalLayout)
+    
+    new Button(actionsPanel)
+      .setCaption("Agregar Cliente")
+      .onClick(new MessageSend(this, "agregarCliente"))
+  }
+ 
+ def agregarCliente() {
+  //  this.openDialog(new AgregarClienteWindow(this, getModelObject))
+ }
+
+ def openDialog(dialog: Dialog[_]) {
+    dialog.onAccept(new MessageSend(getModelObject, "search"))
+    dialog.open
+ }
+}
